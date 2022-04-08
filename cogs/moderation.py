da16dc9ede631938a7e662eb15f4940e8485ffc0
Joystickplays <joystickgaming09@gmail.com>
2022-04-08 14:47:18 +0900

    fix kick and ban

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -15,28 +15,28 @@ class Moderation(commands.Cog):
     @app_commands.describe(member="The member to ban.", reason="The reason for the ban.", notifymember="Whether or not to notify the member.")
     async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str = None, notifymember: bool = True):
         if interaction.user.top_role.position < member.top_role.position:
-            await interaction.message.channel.send(f"{interaction.user.mention}, You cannot ban a member that is higher than you.", ephemeral=True)
+            await interaction.response.send_message(f"{interaction.user.mention}, You cannot ban a member that is higher than you.", ephemeral=True)
             return
 
         await member.ban(reason=reason)
         if notifymember:
             await member.send(f"You have been banned from {interaction.message.guild.name} for {reason}")
         
-        await interaction.message.channel.send(f"{interaction.user.mention}, You have banned {member.mention}.", ephemeral=True)
+        await interaction.response.send_message(f"{interaction.user.mention}, You have banned {member.mention}.", ephemeral=True)
 
     @app_commands.command(description="Kick a member.")
     # @app_commands.guilds(discord.Object(id=956522017983725588))
     @app_commands.describe(member="The member to kick.", reason="The reason for the kick.", notifymember="Whether or not to notify the member.")
     async def kick(self, interaction: discord.Interaction, member: discord.Member, reason: str = None, notifymember: bool = True):
         if interaction.user.top_role.position < member.top_role.position:
-            await interaction.message.channel.send(f"{interaction.user.mention}, You cannot kick a member that is higher than you.", ephemeral=True)
+            await interaction.response.send_message(f"{interaction.user.mention}, You cannot kick a member that is higher than you.", ephemeral=True)
             return
 
         await member.kick(reason=reason)
         if notifymember:
             await member.send(f"You have been kicked from {interaction.message.guild.name} for {reason}")
         
-        await interaction.message.channel.send(f"{interaction.user.mention}, You have kicked {member.mention}.", ephemeral=True)
+        await interaction.response.send_message(f"{interaction.user.mention}, You have kicked {member.mention}.", ephemeral=True)
         
 
 async def setup(bot: commands.Bot):
